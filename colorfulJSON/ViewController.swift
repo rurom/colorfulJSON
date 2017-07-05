99f03728992d041b232fc96d4993f15cf4fa7df2
Roman <noteR@MacBook-Pro-Roman.local>
2017-07-05 21:45:32 +0300

    Initial Commit

---
+++ colorfulJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  colorfulJSON
+//
+//  Created by Roman on 7/5/17.
+//  Copyright © 2017 Swift Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
